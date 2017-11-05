700825036ccc85783d5e31291a61a95277cc7429
Nymann <s144833@student.dtu.dk>
2017-11-05 17:22:34 +0100

    Solution for Problem four added.

---
+++ Problems/ProblemFour.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ProjectEuler.Problems
+{
+    public class ProblemFour
+    {
+        /*
+         * https://projecteuler.net/problem=4
+         * Largest palindrome product
+         * 
+         * A palindromic number reads the same both ways.
+         * The largest palindrome made from the product of two 2-digit numbers
+         * is 9009 = 91 × 99.
+         * 
+         * Find the largest palindrome made from the product of two 3-digit numbers.
+         */
+
+        public ProblemFour()
+        {
+            // double for loop both from 100 to 999
+            // multiply the two numbers, and check if palindrone.
+            //
+
+            var largestPalindroneNumber = 0;
+            for (var i = 999; i >= 100; i--)
+            {
+                for (var j = 999; j >= 100; j--)
+                {
+                    var number = i * j;
+                    if (number < largestPalindroneNumber)
+                    {
+                        break;
+                    }
+
+                    if (IsPalindrone(number))
+                    {
+                        largestPalindroneNumber = number;
+                    }
+                }
+            }
+
+            Console.WriteLine(largestPalindroneNumber);
+        }
+
+        private bool IsPalindrone(long number)
+        {
+            var numberAsString = Convert.ToString(number);
+            var i = numberAsString.Length - 1;
+            var j = 0;
+
+            while (i >= j)
+            {
+                if (!numberAsString[i].Equals(numberAsString[j]))
+                {
+                    return false;
+                }
+
+                j++;
+                i--;
+            }
+
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using ProjectEuler.Problems;
 
 namespace ProjectEuler
@@ -7,10 +8,16 @@ namespace ProjectEuler
     {
         private static void Main(string[] args)
         {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
             //var result = ProblemOne.SumOfMultiples(3, 1000, 3, 5);
             //var result = ProblemTwo.SumOfEvenValuedFibonacciSequenceTerms(4000000);
-            
-            new ProblemThree(600851475143);
+            //new ProblemThree(600851475143);
+            new ProblemFour();
+
+
+            stopWatch.Stop();
+            Console.WriteLine($"\nSolved in {stopWatch.ElapsedMilliseconds} msec.");
             Console.ReadKey(); // So the window doesn't close immidiately.
         }
     }
